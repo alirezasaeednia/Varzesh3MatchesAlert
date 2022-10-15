1571d0f42a7bfbc7a881e24abf1f99f35801d8b6
alireza saeednia <thesimsthelords@gmail.com>
2022-10-15 21:03:31 +0330

    added Jsoup from varzesh3.com and finished

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://varzesh3.com").get();
+
+        Elements div = doc.select("#67");
+        for(Element e:div){
+            String s=e.wholeText().replaceAll("( )", " ");
+            s=s.replaceAll("(  )+", "");
+            System.out.println(s);
+        }
 
     }
 }
